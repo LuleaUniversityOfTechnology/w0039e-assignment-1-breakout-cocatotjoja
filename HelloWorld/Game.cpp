49a219be4b3acc10593424a6b4c4ec9ff4922e94
cocatotjoja <cocatotjoja@gmail.com>
2024-11-09 21:26:01 +0100

    Currently working, step 4

---
+++ HelloWorld/Game.cpp
@@ -0,0 +1,15 @@
+#define PLAY_IMPLEMENTATION
+#define PLAY_USING_GAMEOBJECT_MANAGER
+#include "Play.h"
+#include "Game.h"
+
+void SpawnBall()
+{
+	const int objectId = Play::CreateGameObject(ObjectType::TYPE_BALL, { DISPLAY_WIDTH / 2, DISPLAY_HEIGHT - 60 }, 4, "ball");
+}
+
+void StepFrame(float timePassed)
+{
+	const std::vector<int> ballIds = Play::CollectGameObjectIDsByType(TYPE_BALL);
+	Play::GetGameObject(ballIds);
+}
